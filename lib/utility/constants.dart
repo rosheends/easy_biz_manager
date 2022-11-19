605d8690ce02b2f7e0ac0e6a9f0b4e879bc01d47
rosdlk <rosdlk@ifsworld.com>
2022-11-19 23:24:43 +0530

    Product update and loading view

--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -3,7 +3,7 @@ class Constants {
 
   static const String appName = "Biz Manager";
   static const String appVersion = "1.0.0";
-  static const String serviceHost = "http://172.20.10.7:8080/";
+  static const String serviceHost = "http://172.29.176.1:8080/";
   static const String serviceBasePath = "api/v1/";
 
 }
